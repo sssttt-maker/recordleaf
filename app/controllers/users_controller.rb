137f65d6085a4db672e4b83d2e479f9f82e0bb30
shuhei.takai <stst.shu82@gmail.com>
2019-09-18 19:50:41 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      flash.now[:alert] = "ユーザーの登録に失敗しました。"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      flash.now[:alert] = "ユーザーの更新に失敗しました。"
+      render :edit
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: "ユーザー「#{@user.name}」を削除しました。"
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
